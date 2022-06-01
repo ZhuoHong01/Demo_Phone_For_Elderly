8051031f7c43f372c51403c06f06157164c6d80a
lee zhuo hong <21018193@myrp.edu.sg>
2022-06-01 11:26:05 +0800

    e

--- app/src/main/java/sg/edu/rp/c346/id21018193/demophoneforelderly/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demophoneforelderly/MainActivity.java
@@ -2,6 +2,8 @@ package sg.edu.rp.c346.id21018193.demophoneforelderly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,14 +24,16 @@ public class MainActivity extends AppCompatActivity {
         callSon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel"+92240336));
+                startActivity(intentCall);
             }
         });
 
         callDaughter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel"+89675543));
+                startActivity(intentCall);
             }
         });
     }
