81e0c8528ddd8466624295c22ccfd68c80d62733
lee zhuo hong <21018193@myrp.edu.sg>
2022-06-01 11:22:39 +0800

    e

--- app/src/main/java/sg/edu/rp/c346/id21018193/demophoneforelderly/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demophoneforelderly/MainActivity.java
@@ -3,12 +3,34 @@ package sg.edu.rp.c346.id21018193.demophoneforelderly;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button callSon;
+    Button callDaughter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callSon = findViewById(R.id.buttonCallSon);
+        callDaughter = findViewById(R.id.buttonCallDaughter);
+
+        callSon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        callDaughter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
